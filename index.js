e4dcc5ef4f2f1db5f37f9c8bbc9ea1fa73aecb39
JVRFurtado <joao.furtado546@gmail.com>
2024-11-20 01:43:22 -0300

    2 commit

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('1DESAFIO');
+const nome = "Zéca o Urubu";
+let XP = 5000001;
+
+if (XP <= 1000) {
+    console.log("O Herói de nome " + nome + " está no nível Ferro");
+  } else if (XP <= 2000) {
+    console.log("O Herói de nome " + nome + " está no nível Bronze");
+  } else if (XP <= 5000) {
+    console.log("O Herói de nome " + nome + " está no nível Prata");
+  } else if (XP <= 7000) {
+    console.log("O Herói de nome " + nome + " está no nível Ouro");
+  } else if (XP <= 8000) {
+    console.log("O Herói de nome " + nome + " está no nível Platina");
+  } else if (XP <= 9000) {
+    console.log("O Herói de nome " + nome + " está no nível Ascendente");
+  } else if (XP <= 10000) {
+    console.log("O Herói de nome " + nome + " está no nível Imortal");
+  } else if (10001 <= XP) {
+    console.log("O Herói de nome " + nome + " está no nível Radiante");
+  };
+
